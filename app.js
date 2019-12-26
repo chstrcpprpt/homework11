86bb05324b9ddb5730a514d95f5f177dae7fe461
chstrcpprpt <cjhallen@gmail.com>
2019-12-27 08:53:42 +1100

    put routes into controller.js

--- app.js
+++ app.js
@@ -1,40 +1,18 @@
+// require files
 const express = require("express");
+const controller = require("./controllers/controller");
 
 const app = express();
 
 // setup template engine
 app.set("view engine", "ejs");
 
-// static files
+// static files middleware
 app.use(express.static("./public"));
+
+// fire controllers
+controller(app);
 
 // listen to port
 app.listen(3000);
 console.log("Listening to port 3000");
-
-// HTML routes
-// notes
-app.get("/notes", (req, res) => {
-  res.render("notes");
-});
-
-// *
-app.get("*", (req, res) => {
-  res.render("index");
-});
-
-// API routes
-// GET
-app.get("/api/notes", (req, res) => {
-  
-});
-
-// POST
-app.post("/api/notes", (req, res) => {
-  
-});
-
-// DELETE
-app.delete("/api/notes/:id", (req, res) => {
-
-});
